707a16abd543e30b7da2fe5424f16775221978a7
arunsai2002 <92771726+arunsai2002@users.noreply.github.com>
2023-04-07 16:51:57 +0530

    Create signup.js

---
+++ signup.js
@@ -0,0 +1,42 @@
+import React, { useState } from 'react'
+
+import {auth} from './config/firebase';
+import {createUserWithEmailAndPassword} from 'firebase/auth';
+
+const Post1 = () => {
+    
+    const[email,setEmail] = useState("");
+    const[password,setPassword] = useState("");
+    console.log(auth.currentUser.email);
+
+    const submit = async ()=>{
+        createUserWithEmailAndPassword(auth,email,password).then((userCredential)=>{
+        console.log(userCredential)
+      }).catch((error)=>{
+        console.log(error);
+      })
+    }
+   
+  return (
+   <>
+   <div class="container-fluid">
+   <form onSubmit={submit}>
+  <div class="form-group">
+    
+    <input type="email" placeholder="Enter email" name="email" id="email" value={email}  onChange={(e)=>setEmail(e.target.value)}/>
+   
+  </div>
+  <div class="form-group">
+    
+    <input type="password" placeholder="Password" name='password' id='password' value={password}  onChange={(e)=>setPassword(e.target.value)}/>
+    
+  </div>
+  
+  <button type="submit" class="btn btn-primary">signup</button>
+</form>
+   </div>
+   </>
+  )
+}
+
+export default Post1;
